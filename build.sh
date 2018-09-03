32f65a3b34afa01259502f254c27252fbe7af547
David Dix <david@dixieworld.co.uk>
2018-09-03 23:47:18 +0100

    Add a build.sh script

---
+++ build.sh
@@ -0,0 +1,10 @@
+if [ $# -ne 1 ]; then
+    echo $0: usage: build.sh CONTAINER_VERSION
+    exit 1
+fi
+
+CONTAINER_VERSION=$1
+
+docker build --file latest/Dockerfile --tag node-red-homekit:latest --tag node-red-homekit:$CONTAINER_VERSION .
+# Make sure you have mounted smb://dixnas1.local/home on your iMac
+docker image save -o /Volumes/home/Docker/node-red-homekit.tar node-red-homekit
